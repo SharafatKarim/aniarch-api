262cc3a22df21199b3445bd039403f02c18924f2
Afnan <107838593+theafnansami@users.noreply.github.com>
2022-11-01 17:22:05 +0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+Install Dependencies
+
+- pnpm i
+
+Run your Development server
+
+- pnpm dev
+
+Add .env file
+
+- LIST_VAR=["Your Aniarch Frontend's Domain"]
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
+
+require("dotenv").config();
+
+// Default route
+app.get("/", cors(), (req, res) => {
+  res
+    .status(200)
+    .json("Welcome to Aniarch API, to use the API use the /api route");
+});
+
+var whitelist = JSON.parse(process.env.LIST_VAR);
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+// Import route
+const routes = require("./routes");
+
+// Other Routes
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server started listening in port ${PORT}`);
+});
